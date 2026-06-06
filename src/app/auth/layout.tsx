20c54779a255046e3f507f2303492a1e13c3b309
fonixa codex <fonixacodex@gmail.com>
2026-06-06 23:19:50 +0430

    Update auth layout

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,15 +1,7 @@
-// app/layout.tsx
-import { ThemeProvider } from "@/components/darkmoode/ThemeProvider";
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="fa" dir="rtl">
-      <body>
-       
-        <ThemeProvider>
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <div className="min-h-screen">{children}</div>;
 }
